8ad306cb18d6ad1855f4acf0114b7b03f332e262
Andoni Rodriguez <andoni_rdgz@outlook.com>
2021-05-18 02:22:48 -0500

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="navbar navbar-dark bg-dark">
+      <div class="d-flex ml-auto">
+        <router-link class="btn btn-dark" to="/"> cerrar sesión </router-link>
+      </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark">
+      <div class="d-flex mx-auto">
+        <router-link class="btn btn-dark" to="/"> Proyectos </router-link>
+        <router-link class="btn btn-dark" to="/"> Experiencia </router-link>
+        <router-link class="btn btn-dark" to="/"> Habilidades </router-link>
+        <router-link class="btn btn-dark" to="/"> Contacto </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
